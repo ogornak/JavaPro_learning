7e2151eae710b7dc9aa09629216dd687de58f435
ogornak <ovel2@mail.ru>
2024-01-27 18:49:12 +0300

    Task1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,10 @@
+
 public class Application {
     public static void main(String[] args) {
-        
+        try {
+            TestRunner.runTest(SimpleClass.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
